2bfc0fb77ce9c54abd62ef4c2f9709c54a4ba3a8
Pavithra <kspavithra2495@gmail.com>
2021-04-22 16:00:27 +0200

    Added functionalities to display movie title and movie poster

--- App.js
+++ App.js
@@ -1,28 +1,55 @@
 import React, { useState } from 'react';
-import { TextInput, View, Button } from 'react-native';
+import { TextInput, View, Button, ScrollView, StyleSheet, FlatList, Text, Image} from 'react-native';
 import performQuery from './api-client';
-
-const sayHello = async (t) =>{
-  const res = await performQuery(t);
-  alert("Movie " + t + " has " + res.Response + " " + res.totalResults);
-};
-
 
 const MovieSearch = () => {
 const [searchQuery, setSearchQuery] = useState('');
+const [Data , setData] = useState([]);
+
+const sayHello = async (t) =>{
+  setData([]);
+  const res = await performQuery(t);
+  var input ;
+  var data = [];
+  var uniqueImdb = [];
+
+for (input =0 ; input < res.Search.length ; input++){  
+  if ( ! uniqueImdb.includes(res.Search[input]["imdbID"])) {
+    data.push({ key : res.Search[input]["Title"] , key2 :res.Search[input]["Poster"] , key3 : res.Search[input]["imdbID"]}) ;
+    uniqueImdb.push(res.Search[input]["imdbID"]);
+  }
+} 
+setData(data);
+};
+
 
 return (
 <View style={{alignContent: "center", padding: 100}}>
   <TextInput style={{color: "black"}} onChangeText={(t) => setSearchQuery(t)}></TextInput>
-<Button
-style={{}}
-  placeholder="Type here "
-  onPress={() => sayHello(searchQuery)}
-  defaultValue={searchQuery}
-  title="Search">
+  <Button
+  style={{}}
+    placeholder="Type here "
+    onPress={() => sayHello(searchQuery )}
+    defaultValue={searchQuery}
+    title="Search">
   </Button>
+
+<FlatList
+data = {Data}
+keyExtractor={item => item["key3"]}
+renderItem = {({item}) => (
+<View >
+  <Image source = {{uri :(item["key2"])}}
+  style = {{ width: 200, height: 200 }}
+  />
+  <View >
+    <Text >{`${item["key"]} `} 
+    </Text>
+  </View>
+</View>
+)} 
+/>
 </View>
 );
 }
-
 export default MovieSearch;
